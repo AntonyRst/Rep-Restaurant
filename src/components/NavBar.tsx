93ebb251f83d8413eaa2d3a4c095f758adb3f937
Juan Antonio González <anton.codifica@gmail.com>
2026-08-16 19:12:13 -0600

    feat: crear navbar limpia sin estilos

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,30 @@ function NavBar() {
     <header>
       <nav>
         <ul>
-          <Link to="/">Inicio</Link>
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/contacto">Contacto</Link>
+          </li>
+          <li>
+            <Link to="/galeria">Galería</Link>
+          </li>
+          <li>
+            <Link to="/menu">Menú</Link>
+          </li>
+          <li>
+            <Link to="/nosotros">Nosotros</Link>
+          </li>
+          <li>
+            <Link to="/promociones">Promociones</Link>
+          </li>
+          <li>
+            <Link to="/reservaciones">Reservaciones y Horarios</Link>
+          </li>
+          <li>
+            <Link to="/testimonios">Testimonios</Link>
+          </li>
         </ul>
       </nav>
     </header>
